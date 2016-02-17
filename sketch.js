ade0ffe31330cc6efdfd2b2a69edb3794bf52b84
kredati <scr@wayne.edu>
2016-02-17 12:19:15 -0500

    Complete draft of KeyListener.
    Complete KeyListener with some error handling and fulsome commenting.
    Also, add some friendly comments in index.html.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,44 @@
-var setup = function () {
-  // your code goes here
+var keyDrawer = {
+  drawMostRecentKey: function() {
+    this.drawKeyLetter();
+    this.drawKeyCode();
+  },
+
+  drawKeyLetter: function() {
+    text(key, 30, 60);
+  },
+
+  drawKeyCode: function() {
+    text(keyCode, 30, 100);
+  }
 };
 
-var draw = function () {
+var arrowListener = new KeyListener(keyListenerMap.rightArrow, keyDrawer.drawMostRecentKey, keyDrawer);
+
+var letterListener = new KeyListener('A', keyDrawer.drawMostRecentKey, keyDrawer);
+
+var backspaceListener = new KeyListener(keyListenerMap.backspace, keyDrawer.drawMostRecentKey, keyDrawer);
+
+setup = function () {
   // your code goes here
+  createCanvas(300, 300);
 };
+
+draw = function () {
+  background(200);
+  // your code goes here
+  arrowListener.listen();
+  letterListener.listen();
+  backspaceListener.listen();
+};
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    console.log("UP!!: " + UP_ARROW);
+  } else if (keyCode == DOWN_ARROW) {
+    console.log("DOWN!!: " + DOWN_ARROW);
+  } else {
+    console.log("Key pressed: " + key + ": " + keyCode);
+  }
+  return false; // prevent default
+}
